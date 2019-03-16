aeef6b4ff2e844a7bd1a4cdc2dad2924215b6f8c
Victor Nomwesigwa <31304859+missvicki@users.noreply.github.com>
2019-03-16 18:29:56 +0300

    164593767 Chore: Set up react router (#5)
    - Implement set up react router
    - creates a route folder
    - installs react router
    - adds tests
    
    [Starts 164593767]

---
+++ src/routes/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import App from '../App'
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+const Routes = () => (
+    <Router>
+        <div>
+            <Route path="/" exact component={App} />
+        </div>
+    </Router>
+);
+export default Routes;
+
